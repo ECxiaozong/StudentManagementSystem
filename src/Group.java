39b769eac330e2d40b8e657037cf68583fddb635
ECxiaozong <3564376511@qq.com>
2024-10-15 15:11:09 +0800

    更新小组和学生类的注释信息，增加类的描述及版本信息；调整主类的输出格式。

--- src/Group.java
+++ src/Group.java
@@ -3,7 +3,13 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 
 /**
+ * <p>小组类，用于管理学生及其组长信息。</p>
+ *
+ * <p>小组包含小组名称、学生列表和组长。提供了添加、移除学生，以及获取小组信息的方法。</p>
+ *
  * @author 宗意、马雯钧
+ * @since 2024/10/15
+ * @version 1.1
  */
 
 public class Group {
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         ClassGroup classGroup = new ClassGroup();
         classGroup.defaultClassTemplate();
-
         System.out.println(classGroup.randomGroupName());
         System.out.println(classGroup.randomStudentName());
     }
