7d4c35fd9048af84a86337ddd033ae5280ded355
ECxiaozong <3564376511@qq.com>
2024-10-15 14:48:02 +0800

    重构了ClassGroup类，新增默认班级模板创建方法。删除Function类，简化了小组和班级的管理逻辑。修改Group类，增加了获取组长及设置组长的方法。优化了Main类，采用新的班级创建方式。

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,6 @@ public class Main {
     public static void main(String[] args) {
         ClassGroup classGroup = new ClassGroup();
         classGroup.defaultClassTemplate();
-        // classGroup.printGroups();
-        // classGroup.printStudents();
         System.out.println(classGroup.randomGroupName());
         System.out.println(classGroup.randomStudentName());
     }
