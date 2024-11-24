f285bf224a40ef24de366228e204363849ce7ce0
ECxiaozong <3564376511@qq.com>
2024-11-24 16:01:06 +0800

    添加班级、学生和小组管理相关的类及其方法，包括随机选取学生和小组的功能；新增项目和IDEA配置文件，更新依赖项和忽略文件配置。

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -3,82 +3,10 @@ import java.util.List;
 /**
  * <p>小组包含小组名称、学生列表和组长。提供了添加、移除学生，以及获取小组信息的方法。</p>
  *
- * @author 宗意、马雯钧
+ * @author 马雯钧
  * @since 2024-10-15
  * @version 1.0
  */
-
 public class Student {
 
-    /**
-     * 学生姓名
-     */
-    private String name;
-
-    /**
-     * 学号
-     */
-    private String id;
-
-    /**
-     * 性别
-     */
-    private String sex;
-
-    /**
-     * 分数
-     */
-    private int score;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "name='" + name + '\'' +
-                ", id='" + id + '\'' +
-                ", sex='" + sex + '\'' +
-                ", score=" + score +
-                '}';
-    }
-
-    public Student() {
-    }
-
-    public Student(String name, String id, String sex) {
-        this.name = name;
-        this.id = id;
-        this.sex = sex;
-        score = 0;
-    }
 }
