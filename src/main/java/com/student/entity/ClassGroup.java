6960b6d5650f23cd61f532b0c091da32cc44e5a2
HJMPGL <3424631728@qq.com>
2024-11-25 13:51:30 +0800

    增加了对班级和小组的管理功能，包括班级和小组的构造函数、添加和移除学生及小组的方法；新建DAO类用于数据访问；新增学生类的属性和方法；更新了主类以展示新的管理界面；添加了代码检查配置文件。

---
+++ src/main/java/com/student/entity/ClassGroup.java
@@ -0,0 +1,88 @@
+package com.student.entity;
+
+import java.util.ArrayList;
+
+/**
+ * @author 宗意
+ */
+public class ClassGroup {
+    int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * 班级名称
+     */
+    private String name;
+
+    /**
+     * 班级学生列表
+     */
+    private ArrayList<Student> students = new ArrayList<>();
+
+    /**
+     * 班级小组列表
+     */
+    private ArrayList<Group> groups = new ArrayList<>();
+
+    public ClassGroup() {
+    }
+
+    public ClassGroup(String name) {
+        this.name = name;
+    }
+
+    public ClassGroup(String name, ArrayList<Student> students, ArrayList<Group> groups) {
+        this.name = name;
+        this.students = students;
+        this.groups = groups;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public ArrayList<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(ArrayList<Group> groups) {
+        this.groups = groups;
+    }
+
+    public void addGroup(Group group) {
+        if (!groups.contains(group)) {
+            groups.add(group);
+            for (Student student : group.getStudents()) {
+                if (!students.contains(student)) {
+                    students.add(student);
+                }
+            }
+        }
+    }
+
+    public void removeGroup(Group group) {
+        groups.remove(group);
+        for (Student student : group.getStudents()) {
+            students.remove(student);
+        }
+    }
+}
---
+++ src/main/java/com/student/entity/Group.java
@@ -0,0 +1,105 @@
+package com.student.entity;
+
+
+import java.util.ArrayList;
+
+/**
+ * <p>小组包含小组名称、学生列表和组长。提供了添加、移除学生，以及获取小组信息的方法。</p>
+ *
+ * @author 宗意
+ * @since 2024/10/15
+ * @version 1.1
+ */
+
+public class Group {
+
+    private int id;
+
+    /**
+     * 小组名称
+     */
+    private String name;
+
+    private ArrayList<Student> students = new ArrayList<>();
+
+    public Group() {
+    }
+
+    public Group(String name) {
+        this.name = name;
+    }
+
+    public Group(String name, ArrayList<Student> students) {
+        this.name = name;
+        this.students = students;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public int getSize() {
+        return students.size();
+    }
+
+    public void addStudent(Student student) {
+        if (!students.contains(student)) {
+            students.add(student);
+        }
+    }
+
+    public void removeStudent(Student student) {
+        students.remove(student);
+    }
+
+    public void removeStudentByName(String name) {
+        for (Student student : students) {
+            if (student.getName().equals(name)) {
+                students.remove(student);
+                return;
+            }
+        }
+    }
+
+    public void removeStudentById(String id) {
+        for (Student student : students) {
+            if (student.getId().equals(id)) {
+                students.remove(student);
+                return;
+            }
+        }
+    }
+
+    public Student randomStudent() {
+        int index = (int) (Math.random() * students.size());
+        return students.get(index);
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "name='" + name + '\'' +
+                ", students=" + students +
+                '}';
+    }
+}
