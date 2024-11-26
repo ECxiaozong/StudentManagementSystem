979f76efa52539d2fb49878b7afce68525b192db
ECxiaozong <3564376511@qq.com>
2024-11-26 10:29:59 +0800

    优化文件名称比较逻辑。

--- src/main/java/com/student/view/StudentListPanel.java
+++ src/main/java/com/student/view/StudentListPanel.java
@@ -83,7 +83,7 @@ public class StudentListPanel extends JPanel {
         file = new File(Constant.FILE_PATH + "/" + Constant.CLASS_PATH);
         File files[] = file.listFiles();
         for (File file1 : files) {
-            if (!file1.getName().equals("students.txt")) {
+            if (!"students.txt".equals(file1.getName())) {
                 cmbGroup.addItem(file1.getName().substring(0, file1.getName().length() - 4));
             }
         }
